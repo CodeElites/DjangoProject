0e8549cd5897babebae86134054fb085125dde95
onkarmadhekar <onkarmadhekar22@gmail.com>
2021-03-10 09:35:21 +0530

    Model Destination Created

--- SafarProject/settings.py
+++ SafarProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Safar',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'SafarProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/orcl',
+        'USER': 'c##admin',
+        'PASSWORD': 'admin',
     }
 }
 
